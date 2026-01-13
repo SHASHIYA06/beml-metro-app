770fa2a80233da9098c2ce2b8a458c4df039ea4e
SHASHIYA06 <shashiaaidu@gmail.com>
2026-01-13 12:31:56 +0530

    fix: target ES2020 to avoid regeneratorRuntime error

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    target: 'es2020',
+    minify: 'esbuild'
+  },
+  esbuild: {
+    target: 'es2020'
+  }
 })
